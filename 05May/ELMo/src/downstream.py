d3d50154931df8a05d9bdf9d9db82cfe97187560
gyuminjack <sld9849@gmail.com>
2021-05-25 15:44:36 +0900

    modify

--- 05May/ELMo/src/downstream.py
+++ 05May/ELMo/src/downstream.py
@@ -4,6 +4,49 @@ import torch.nn.functional as F
 import numpy as np
 from gensim.models import KeyedVectors
 
+class BaseGRUClassifier(nn.Module):
+    def __init__(self, vocab, emb_dim, hid_dim, output_dim, w2v_path, n_layers = 2, dropout_rate = 0.5, pad_idx = 0, device = 'cuda:1'):
+        super(BaseGRUClassifier, self).__init__()
+        vocab_size = len(vocab)
+        self.embedding_static = nn.Embedding(vocab_size, emb_dim, padding_idx=pad_idx)
+        self.embs = [self.embedding_static]
+
+        self.rnn = nn.GRU(emb_dim, hid_dim, num_layers=n_layers, bidirectional = True)
+        self.fc = nn.Linear(hid_dim*2, output_dim)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.device = device
+        if w2v_path is not None:
+            self._init_embedding_vectors(w2v_path, vocab)
+            self.embedding_static.weight.requires_grad = False
+ 
+    def forward(self, tokens):
+        independent_input = tokens[1].to(self.device)
+        embedding = self.embedding_static(independent_input)
+        output, hidden = self.rnn(embedding)
+        # output = (seq_len, batch, num_directions * hidden_size)
+        last_hiddens = torch.cat([hidden[-2, :, :], hidden[-1, :, :]], dim = -1)
+        fc_out = self.fc(last_hiddens)
+        return fc_out
+
+    @classmethod
+    def make_init_vectors(cls, w2v_path, vocab_dict):
+        w2v_model = KeyedVectors.load_word2vec_format(w2v_path)
+        lower = -(np.std(w2v_model.vectors, axis=0) ** 2)
+        upper = lower * -1
+        my_data_vectors = []
+        w2v_tokens = w2v_model.vocab.keys()
+        for token in vocab_dict:
+            if token in w2v_tokens:
+                my_data_vectors.append(torch.FloatTensor(np.copy(w2v_model[token])))
+            else:
+                my_data_vectors.append(torch.FloatTensor(np.random.uniform(lower, upper)))
+        stacked_data = torch.stack(my_data_vectors)
+        return stacked_data
+
+    def _init_embedding_vectors(self, w2v_path, my_vocabs):
+        vectors_in_trainset = self.make_init_vectors(w2v_path, my_vocabs.vocab_dict.keys())
+        [emb.weight.data.copy_(vectors_in_trainset) for emb in self.embs]
+
 def get_hiddens(model, input_batch, nlayers=2):
     hiddens = []
     embs = []
@@ -64,18 +107,16 @@ class GRUClassifier(nn.Module):
         if elmo_projection_dim == -1:
             elmo_projection_dim = elmo_hidden_output_dim
 
-        if use_idp_emb == True:
-            self.embedding_static = nn.Embedding(vocab_size, emb_dim, padding_idx=pad_idx)
-            self.embedding_static.weight.requires_grad = False
-            self.embs = [self.embedding_static]
-
-        if w2v_path is not None:
-            self.w2v_init = True
+        if use_idp_emb:
+            self.use_w2v = True
+            self.idp_embedding = nn.Embedding(vocab_size, emb_dim, padding_idx=pad_idx)
+            self.embs = [self.idp_embedding]
             self.rnn = nn.GRU(elmo_projection_dim + emb_dim, hid_dim, num_layers=n_layers, bidirectional = True)
             if w2v_path != 'random':
                 self._init_embedding_vectors(w2v_path, elmo_train_vocab)
+                self.idp_embedding.weight.requires_grad = False
         else:
-            self.w2v_init = False
+            self.use_w2v = False
             self.rnn = nn.GRU(elmo_projection_dim, hid_dim, num_layers=n_layers, bidirectional = True)
 
         self.fc = nn.Linear(hid_dim*2, output_dim)
@@ -86,8 +127,8 @@ class GRUClassifier(nn.Module):
         elmo_hiddens = get_hiddens(self.elmo, elmo_character_input)
         elmo_embeddings = self.task_emb(elmo_hiddens).to(self.device)
 
-        if self.w2v_init:
-            independent_embeddings = self.embedding_static(independent_input).to(self.device)
+        if self.use_w2v:
+            independent_embeddings = self.idp_embedding(independent_input).to(self.device)
             embedding_concat = torch.cat([elmo_embeddings, independent_embeddings], dim = -1)
         else:
             embedding_concat = elmo_embeddings
@@ -116,48 +157,3 @@ class GRUClassifier(nn.Module):
     def _init_embedding_vectors(self, w2v_path, my_vocabs):
         vectors_in_trainset = self.make_init_vectors(w2v_path, my_vocabs.vocab_dict.keys())
         [emb.weight.data.copy_(vectors_in_trainset) for emb in self.embs]
-
-class BaseGRUClassifier(nn.Module):
-    def __init__(self, vocab, emb_dim, hid_dim, output_dim, w2v_path, n_layers = 2, dropout_rate = 0.5, pad_idx = 0, device = 'cuda:1'):
-        super(BaseGRUClassifier, self).__init__()
-        vocab_size = len(vocab)
-        self.embedding_static = nn.Embedding(vocab_size, emb_dim, padding_idx=pad_idx)
-        self.embedding_static.weight.requires_grad = False
-        self.embs = [self.embedding_static]
-
-        self.rnn = nn.GRU(emb_dim, hid_dim, num_layers=n_layers, bidirectional = True)
-        self.fc = nn.Linear(hid_dim*2, output_dim)
-        self.dropout = nn.Dropout(dropout_rate)
-        self.device = device
-        if w2v_path is not None:
-            self._init_embedding_vectors(w2v_path, vocab)
- 
-    def forward(self, tokens):
-        independent_input = tokens[1].to(self.device)
-        embedding = self.embedding_static(independent_input)
-        output, hidden = self.rnn(embedding)
-        # output = (seq_len, batch, num_directions * hidden_size)
-        last_hiddens = torch.cat([hidden[-2, :, :], hidden[-1, :, :]], dim = -1)
-        fc_out = self.fc(last_hiddens)
-        return fc_out
-
-    @classmethod
-    def make_init_vectors(cls, w2v_path, vocab_dict):
-        w2v_model = KeyedVectors.load_word2vec_format(w2v_path)
-        lower = -(np.std(w2v_model.vectors, axis=0) ** 2)
-        upper = lower * -1
-        my_data_vectors = []
-        w2v_tokens = w2v_model.vocab.keys()
-        for token in vocab_dict:
-            if token in w2v_tokens:
-                my_data_vectors.append(torch.FloatTensor(np.copy(w2v_model[token])))
-            else:
-                my_data_vectors.append(torch.FloatTensor(np.random.uniform(lower, upper)))
-        stacked_data = torch.stack(my_data_vectors)
-        return stacked_data
-
-    def _init_embedding_vectors(self, w2v_path, my_vocabs):
-        vectors_in_trainset = self.make_init_vectors(w2v_path, my_vocabs.vocab_dict.keys())
-        [emb.weight.data.copy_(vectors_in_trainset) for emb in self.embs]
-
-
